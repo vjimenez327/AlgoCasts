a57829bf08fddc31f51ae804d1116344c9c900f2
Vanessa Jiménez <16891417+vjimenez327@users.noreply.github.com>
2018-06-05 17:45:00 -0400

    added linkedlist constructor and a few methods

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -3,12 +3,34 @@
 // See 'directions' document
 
 class Node {
-  constructor(data, next = null){
+  constructor(data, next = null) {
     this.data = data;
     this.next = next;
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  } 
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size(){
+    let count = 0; 
+    let node  = this.head;
+
+    while (node) {
+      count++;
+      node = node.next;
+    }
+
+    return count;
+  }
+
+}
+
 
 module.exports = { Node, LinkedList };
