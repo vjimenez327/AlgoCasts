f0c798d9d817580b632a4d1229b543c7fb403bea
Vanessa Jiménez <16891417+vjimenez327@users.noreply.github.com>
2018-06-06 16:06:59 -0400

    clear method

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -30,6 +30,30 @@ class LinkedList {
     return count;
   }
 
+  getFirst(){
+    return this.head;
+  }
+
+  getLast(){
+
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+
+  clear() {
+    this.head = null;
+  }
+
 }
 
 
